05708bd97f75ed80b2df555b5736c6fc2f2d02e1
Soemon007 <rehanmallik31@gmail.com>
2026-02-06 01:39:55 +0530

    Clarification

--- Kelp_ai_teaser 2/scripts/load_private_data.py
+++ Kelp_ai_teaser 2/scripts/load_private_data.py
@@ -1,13 +1,15 @@
+#Imports to circumvent fatal errors
 import warnings
 warnings.filterwarnings("ignore")
 
-
+#Function reads .md file to collect data for API
 def load_private_data(file_name):
   FILE_TO_READ = file_name
   try:
       with open(FILE_TO_READ, "r", encoding="utf-8") as f:
           file_content = f.read()
   except FileNotFoundError:
+      #Fallback if no file found
       print("File not found.")
       return
   return file_content
